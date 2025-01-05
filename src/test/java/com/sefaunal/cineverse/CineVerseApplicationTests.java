2d9410f294e575353ffe1aac496a9a40dfb25980
sefaunal <83312431+sefaunal@users.noreply.github.com>
2025-01-05 16:41:42 +0300

    Initial Commit

---
+++ src/test/java/com/sefaunal/cineverse/CineVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sefaunal.cineverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
